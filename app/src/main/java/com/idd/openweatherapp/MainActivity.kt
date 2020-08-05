b86f99d53e428cdaa6c77aca955b2b492315c957
Ignacio Deandreis Denis <ignacio@astropay.com>
2020-08-05 16:13:01 -0300

    created project

---
+++ app/src/main/java/com/idd/openweatherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.idd.openweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
