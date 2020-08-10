f0d3a431ab66d8329784b8d34b7cf1e9a23d156f
Ignacio Deandreis Denis <ignacio@astropay.com>
2020-08-10 11:17:25 -0300

    [wip] added hilt dependencies

---
+++ app/src/main/java/com/idd/openweatherapp/api/WeatherApi.kt
@@ -0,0 +1,13 @@
+package com.idd.openweatherapp.api
+
+import androidx.lifecycle.LiveData
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface WeatherApi {
+
+//    @GET("users/{login}")
+//    fun getUser(@Path("login") login: String): LiveData<ApiResponse<User>>
+
+
+}
