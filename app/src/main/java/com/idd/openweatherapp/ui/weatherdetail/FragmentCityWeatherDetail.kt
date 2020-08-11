d87b5c1a1aaa7c9829eab9170fdc6f4091f6dfde
Ignacio Deandreis Denis <ignacio@astropay.com>
2020-08-11 13:40:26 -0300

    [wip] added glide for images

--- app/src/main/java/com/idd/openweatherapp/ui/weatherdetail/FragmentCityWeatherDetail.kt
+++ app/src/main/java/com/idd/openweatherapp/ui/weatherdetail/FragmentCityWeatherDetail.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.idd.openweatherapp.R
 import com.idd.openweatherapp.model.CurrentWeather
 import dagger.hilt.android.AndroidEntryPoint
@@ -51,6 +52,13 @@ class FragmentCityWeatherDetail : Fragment() {
         Log.e("Updating", currentWeather?.name ?: "null")
 
         currentWeather?.let {
+            Glide
+                .with(this)
+                .load(getIcon(currentWeather.weather[0].icon))
+                .centerCrop()
+                .placeholder(R.drawable.svg_splash_icon)
+                .into(fragment_city_weather_detail_image_view)
+
             fragment_city_weather_detail_city_name_text_view.text = currentWeather.name
 
             fragment_city_weather_detail_weather_description_text_view.text =
@@ -98,6 +106,10 @@ class FragmentCityWeatherDetail : Fragment() {
             )
         }
 
+    }
+
+    private fun getIcon(icon: String): String {
+        return "http://openweathermap.org/img/wn/${icon}@2x.png"
     }
 
     private var nf: DecimalFormat? = null
